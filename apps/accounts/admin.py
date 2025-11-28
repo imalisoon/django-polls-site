cc62c4a05fc4e2f6c4257d28fea4831787db04a7
imalisoon <alisondasilva291103@gmail.com>
2025-11-28 13:38:33 -0300

    feat: create the accounts app and rewirite the user model

---
+++ apps/accounts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+
+
+class UserModelAdmin(UserAdmin):
+    pass
+
+
+admin.site.register(User, UserModelAdmin)
+
